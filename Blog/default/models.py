b2037338374e98cd3a80a905df44d1ddc30a9122
Shamil K Muhammed <necessary129@users.noreply.github.com>
2016-05-19 19:18:00 +0530

    Post model
    Created the very first model

--- Blog/default/models.py
+++ Blog/default/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    subtitle = models.CharField(max_length=300)
+    content = models.TextField()
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.title
