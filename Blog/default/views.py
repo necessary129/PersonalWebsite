36768e0c529f127fd066213e27654506032d0d56
Shamil K Muhammed <necessary129@users.noreply.github.com>
2016-05-19 19:13:37 +0530

    Created views
    Created views, only index works for the moment

--- Blog/default/views.py
+++ Blog/default/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import Http404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+from .models import Post
 
 # Create your views here.
+
+def index(request, page):
+    posts = Post.objects.all()
+    paginator = Paginator(posts, 10)
+    if not page:
+        posts = paginator.page(1)
+    else:
+        try:
+            posts = paginator.page(int(page))
+        except EmptyPage:
+            raise Http404("Page does not exist.")
+    return render(request, 'default/index.html', {'posts': posts})
+
+
+def contact(request):
+    pass
+
+def about(request):
+    pass
+
+def post(request, slug):
+    pass
