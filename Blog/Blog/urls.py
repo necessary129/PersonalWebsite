a5a22c69d9f5ec3fdf392bd6c295c8a2b26f581b
Shamil K Muhammed <necessary129@users.noreply.github.com>
2016-05-19 13:31:26 +0530

    Added url for index URL for index page and and the paginated ones.

--- Blog/Blog/urls.py
+++ Blog/Blog/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from default import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^(?:page/(?P<page>\d+)/)?$', views.index, name="home"),
 ]
