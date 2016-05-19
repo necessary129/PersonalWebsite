fe258ac1447ad19a9f22a187670c732b76bc147e
Shamil K Muhammed <necessary129@users.noreply.github.com>
2016-05-19 19:51:49 +0530

    Better admin
    configured site_* 
    added filters and better display

--- Blog/default/admin.py
+++ Blog/default/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 from .models import Post
 # Register your models here.
+admin.site.site_header = "Blog administration"
+admin.site.sit_title = "Blog administration"
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
+    list_display = ['title', 'pub_date', 'draft']
+    list_filter = ['draft', 'pub_date']
+
 admin.site.register(Post, PostAdmin)
