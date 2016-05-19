2e25286a350b2a891bdc382485cdf4fb7c54ef22
Shamil K Muhammed <necessary129@users.noreply.github.com>
2016-05-19 21:16:36 +0530

    Improvements
    Made the markup be processed when saving, made a custom slug field. Gave everything names.

--- Blog/default/models.py
+++ Blog/default/models.py
@@ -1,16 +1,28 @@
 from django.db import models
 
+import markdown
+import re
 # Create your models here.
+slre = re.compile(r'[^a-zA-Z0-9]+')
+def slugify(txt):
+    return slre.sub('-', txt).lower()
+
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200)
-    subtitle = models.CharField(max_length=300)
-    content = models.TextField()
-    pub_date = models.DateTimeField('date published')
+    title = models.CharField("Title", max_length=200)
+    slug = models.SlugField(max_length=200,blank=True, null=True, unique=True)
+    subtitle = models.CharField("Sub Title", max_length=300)
+    content = models.TextField('Entry body', help_text='Use Markdown syntax.')
+    pub_date = models.DateTimeField('Date Published')
+    body = models.TextField('Entry body as HTML', blank=True, null=True)
 
     def bool_d():
         return False
     draft = models.BooleanField(default=bool_d)
+
+    def save(self):
+        self.body = markdown.markdown(self.content)
+        self.slug = slugify(self.title)
+        super().save()
 
     def __str__(self):
         return self.title
