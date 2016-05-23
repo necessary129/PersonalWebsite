6f2f135db7c3d4aa41dacfa0a0ae6af4576c430b
Shamil K Muhammed <necessary129@users.noreply.github.com>
2016-05-23 15:27:55 +0530

    Missed draft & typo

--- Blog/default/admin.py
+++ Blog/default/admin.py
@@ -3,12 +3,12 @@ from django.contrib import admin
 from .models import Post
 # Register your models here.
 admin.site.site_header = "Blog administration"
-admin.site.sit_title = "Blog administration"
+admin.site.site_title = "Blog administration"
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'pub_date', 'draft']
     list_filter = ['draft', 'pub_date']
     fieldsets = (
-    (None, {'fields': ('title', 'content', 'pub_date', 'draft')}),
+    (None, {'fields': ('title', 'subtitle', 'content', 'pub_date', 'draft')}),
     )
 
 admin.site.register(Post, PostAdmin)
