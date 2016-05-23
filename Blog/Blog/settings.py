755178fd79501875e0e91894d75d9870e8f68208
Shamil K Muhammed <necessary129@users.noreply.github.com>
2016-05-23 15:57:38 +0530

    setting template tag
    Added a template tag by which we can access settings.py.

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -120,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+from django import template
+
+register = template.Library()
+
+@register.simple_tag
+def setting(name):
+    return getattr(globals(), name, "")
