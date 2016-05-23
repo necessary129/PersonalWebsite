b9c719de35e06069fff05cc29acff085aadb810b
Shamil K Muhammed <necessary129@users.noreply.github.com>
2016-05-23 15:29:53 +0530

    Remove width limit(?)

--- Blog/default/static/default/js/site.js
+++ Blog/default/static/default/js/site.js
@@ -3,10 +3,7 @@ $(document).ready(function() {
 });
 
 jQuery(document).ready(function($) {
-    var MQL = 1170;
 
-    //primary navigation slide-in effect
-    if ($(window).width() > MQL) {
         var headerHeight = $('.navbar-custom').height();
         $(window).on('scroll', {
                 previousTop: 0
@@ -28,5 +25,4 @@ jQuery(document).ready(function($) {
                 }
                 this.previousTop = currentTop;
             });
-    }
 });
