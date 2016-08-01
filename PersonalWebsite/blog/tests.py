f49456b395da06accb7f657081711ff9bb8a6ef2
Shamil K Muhammed <necessary129@users.noreply.github.com>
2016-08-01 12:37:13 +0530

    Add a partial test. For now

--- PersonalWebsite/blog/tests.py
+++ PersonalWebsite/blog/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.test import Client
+
+from django.utils import timezone
+
+from .models import Post, Tag
+
+
 
 # Create your tests here.
+class DefaultTest(TestCase):
+    def setUp(self):
+        self.c = Client()
+
+    def test_relations(self):
+        tags = [Tag.objects.create(name=str(i)) for i in range(10)]
+        for tg in tags:
+            tg.save()
+        pst = Post.objects.create(title='Test',subtitle='Test',content="# Head",pub_date=timezone.now())
+        pst.save()
+        pst.tags.add(*tags)
+        self.assertEqual(list(pst.tags.all()), tags)
+        for tg in tags:
+            self.assertEqual(list(tg.post_set.all()), [pst])
+
+    
